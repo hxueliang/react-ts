154de56e0ce721b7390d8458146f361b4cbe8c05
九级浪 <413612464@qq.com>
2023-12-08 17:14:48 +0800

    feat: JIKI案例-安装antd-mobile

--- src/App/10-JIKI案例.tsx
+++ src/App/10-JIKI案例.tsx
@@ -1,9 +1,18 @@
 /**
  * 10-JIKI案例
+ *
+ * 1.安装antd-mobile
  */
 
+import { Button } from 'antd-mobile'
+
 function App() {
-  return <>this is app</>
+  return (
+    <>
+      this is app
+      <Button color='success'>click</Button>
+    </>
+  )
 }
 
 export default App
