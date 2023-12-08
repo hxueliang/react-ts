51137dce14455d9d5aa1ddd8d6a3b0b4cc5f3acc
九级浪 <413612464@qq.com>
2023-12-08 16:10:01 +0800

    init: props-基础使用

---
+++ src/App/04-props-基础使用.tsx
@@ -0,0 +1,9 @@
+/**
+ * 04-props-基础使用
+ */
+
+function App() {
+  return <>this is app</>
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import ReactDOM from 'react-dom/client'
-import App from './App/03-useState-初始值为null'
+import App from './App/04-props-基础使用'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
