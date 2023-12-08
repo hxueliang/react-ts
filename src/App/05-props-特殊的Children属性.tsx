c23c638987eecdc79b33f99d9d601880048d138d
九级浪 <413612464@qq.com>
2023-12-08 16:24:17 +0800

    feat: props-特殊的Children属性

--- src/App/05-props-特殊的Children属性.tsx
+++ src/App/05-props-特殊的Children属性.tsx
@@ -2,8 +2,38 @@
  * 05-props-特殊的Children属性
  */
 
+type Props = {
+  className: string
+  children: React.ReactNode
+  /**
+   * 注解之后，
+   * children可以是多种类型：
+   * React.ReactElement
+   * React.ReactFragment
+   * React.ReactPortal
+   * string
+   * number
+   * boolean
+   * null
+   * undefined
+   */
+}
+
+function Button(prpos: Props) {
+  const { className, children } = prpos
+  return <button className={className}>{children}</button>
+}
+
 function App() {
-  return <>this is app</>
+  return (
+    <>
+      this is app
+      <Button className='btn'>
+        {/* <span>click me</span> */}
+        click me
+      </Button>
+    </>
+  )
 }
 
 export default App
