181e4401f518ee7cc43149f8125833b854921974
九级浪 <413612464@qq.com>
2023-12-08 17:07:10 +0800

    init: JIKI案例

---
+++ src/App/10-JIKI案例.tsx
@@ -0,0 +1,9 @@
+/**
+ * 10-JIKI案例
+ */
+
+function App() {
+  return <>this is app</>
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import App from './App/08-useRef-引用稳定的存储器'
+import App from './App/10-JIKI案例'
+
+// 10-JIKI案例
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
