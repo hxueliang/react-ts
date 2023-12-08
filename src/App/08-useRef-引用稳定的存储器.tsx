9a664dd6f6c280b677e65a9aa1a54f93f2bd5262
九级浪 <413612464@qq.com>
2023-12-08 17:02:11 +0800

    init: useRef-引用稳定的存储器

---
+++ src/App/08-useRef-引用稳定的存储器.tsx
@@ -0,0 +1,9 @@
+/**
+ * 08-useRef-引用稳定的存储器
+ */
+
+function App() {
+  return <>this is app</>
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import ReactDOM from 'react-dom/client'
-import App from './App/07-useRef-获取dom'
+import App from './App/08-useRef-引用稳定的存储器'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
