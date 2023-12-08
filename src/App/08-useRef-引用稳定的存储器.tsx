cdd5ad0c5a288bce24ca70a50c24254eb1284e99
九级浪 <413612464@qq.com>
2023-12-08 17:02:19 +0800

    feat: useRef-引用稳定的存储器

--- src/App/08-useRef-引用稳定的存储器.tsx
+++ src/App/08-useRef-引用稳定的存储器.tsx
@@ -1,8 +1,22 @@
 /**
  * 08-useRef-引用稳定的存储器
+ *
+ * 可以通过泛型传入联合类型来做，如定时器场景
  */
 
+import { useEffect, useRef } from 'react'
+
 function App() {
+  const timeId = useRef<number | undefined>(undefined)
+
+  useEffect(() => {
+    timeId.current = setInterval(() => {
+      console.log(1)
+    }, 1000)
+
+    return () => clearInterval(timeId.current)
+  }, [])
+
   return <>this is app</>
 }
 
