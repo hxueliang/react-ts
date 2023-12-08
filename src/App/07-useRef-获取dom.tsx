30c49d996eadd8b5d422147209006029df0ebe43
九级浪 <413612464@qq.com>
2023-12-08 16:53:59 +0800

    feat: useRef-获取dom

--- src/App/07-useRef-获取dom.tsx
+++ src/App/07-useRef-获取dom.tsx
@@ -1,9 +1,27 @@
 /**
  * 07-useRef-获取dom
+ *
+ * 获取dom的场景，
+ * 可以直接反要获取的dom元素的类型，
+ * 当成泛型参数传递给useRef，
+ * 可以推导出.current属性的类型
  */
 
+import { useEffect, useRef } from 'react'
+
 function App() {
-  return <>this is app</>
+  const domRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    domRef.current?.focus()
+  }, [])
+
+  return (
+    <>
+      this is app
+      <input ref={domRef} />
+    </>
+  )
 }
 
 export default App
