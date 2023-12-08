174e76986ee3d61f355707007c78d2f955a4681b
九级浪 <413612464@qq.com>
2023-12-08 16:22:32 +0800

    init: props-特殊的Children属性

---
+++ src/App/05-props-特殊的Children属性.tsx
@@ -0,0 +1,9 @@
+/**
+ * 05-props-特殊的Children属性
+ */
+
+function App() {
+  return <>this is app</>
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import ReactDOM from 'react-dom/client'
-import App from './App/04-props-基础使用'
+import App from './App/05-props-特殊的Children属性'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
