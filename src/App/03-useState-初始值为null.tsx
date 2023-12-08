2b5adebb56ee2f96363a05c4bd1e3898ccf5f973
九级浪 <413612464@qq.com>
2023-12-08 15:51:34 +0800

    feat: useState-初始值为null

--- src/App/03-useState-初始值为null.tsx
+++ src/App/03-useState-初始值为null.tsx
@@ -9,7 +9,21 @@ type User = {
 }
 
 function App() {
-  return <>this is app</>
+  // 1.当我们不知道状态的初始值是什么，将useState初始值设为null是一个常见的做法，可以通过具体类型联合null来做显式注解
+  // 1.1.限制useState函数参数的初始值可以是User | null
+  // 1.2.限制setUser函数的参数类型可以是User | null
+  const [user, setUser] = useState<User | null>(null)
+
+  const changeUser = () => {
+    setUser({ name: 'hxl', age: 19 })
+  }
+
+  return (
+    <>
+      this is app - {user?.age}
+      <button onClick={changeUser}>{JSON.stringify(user)}</button>
+    </>
+  )
 }
 
 export default App
